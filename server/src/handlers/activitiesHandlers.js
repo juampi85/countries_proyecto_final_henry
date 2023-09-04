6775d6554f2dd639cd77ddbd335227192ad109af
Juan Pablo Accinelli <juanpaccinelli@gmail.com>
2023-09-04 18:10:26 -0300

    Corrige un pequeño error en el activitiesHandler

--- server/src/handlers/activitiesHandlers.js
+++ server/src/handlers/activitiesHandlers.js
@@ -15,7 +15,8 @@ const getActivitiesHandler = async (req, res) => {
 const createActivityHandler = async (req, res) => {
   try {
     const { name, difficulty, duration, season, countries } = req.body;
-    const countryArray = countries.split(',').map((country) => country.trim());
+    // const countryArray = countries.split(',').map((country) => country.trim());
+    const countryArray = countries.map((country) => country.trim());
 
     const countryRecords = await Country.findAll({
       where: {
